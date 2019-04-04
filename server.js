e6c976309a6074680f1f081bc7edf3fd353ca7ea
dinartec <41605725+dinartec@users.noreply.github.com>
2019-04-04 10:25:09 -0600

    test owo

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host     : 'us-cdbr-iron-east-03.cleardb.net',
+    user     : 'b7282d3c829f44',
+    password : '49ad92bf',
+    database : 'heroku_8e679e6d32fb43a'
+})
+
+connection.connect();
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    connection.query('select * from owo', function (err, rows, fields) {
+      if (err) {
+        console.log('error:', err);
+        throw err;
+      }
+      response.send(['owo', rows])
+    })
+    // res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(process.env.PORT || 4000, function(){
