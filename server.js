fd97a1c0eab08ef5837118e8479b93f9aef599a7
dinartec <41605725+dinartec@users.noreply.github.com>
2019-04-04 22:48:06 -0600

    Resolver error con connection

--- server.js
+++ server.js
@@ -10,22 +10,23 @@ const connection = mysql.createConnection({
     database : 'heroku_8e679e6d32fb43a'
 })
 
-connection.connect();
-
 app.use('/post', bodyParser.json());
 app.use('/post', bodyParser.urlencoded({ extended: true }));
+app.set('port', process.env.PORT || 4020)
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/post', (req, res) => {
+  connection.connect();
   const {Nombre, Apellidos, Cargo, Tratamiento, FechaNacimiento, FechaContratacion, Direccion, Ciudad, Region, CodPostal, Pais, TelDomicilio, Extension, Notas, Jefe} = req.body;
   
   connection.query('select IdEmpleado from empleados order by IdEmpleado desc limit 0, 1;', (error, results, fields) => {
     if (error) {
       res.status(400).send("unable to save to database");
       throw error;
+      connection.end();
     }
     const newId = results[0].IdEmpleado + 1;
     const mysqlQuery = `INSERT INTO empleados ( IdEmpleado, Nombre, Apellidos, Cargo, Tratamiento, FechaNacimiento, FechaContratación, Dirección, Ciudad, Región, CódPostal, País, TelDomicilio, Extensión, Notas, Jefe)
@@ -37,8 +38,10 @@ app.post('/post', (req, res) => {
                         if (error) {
                           res.status(400).send("unable to save to database");
                           throw error;
+                          connection.end();
                         }
                         res.send("item saved to database");
+                        connection.end();
                       });
             });
 
