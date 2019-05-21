3cb506b5a60059ca297aa5c3940e4502ec6b5569
dinartec <41605725+dinartec@users.noreply.github.com>
2019-05-21 01:08:38 -0600

    Fix el bug del readfilesync del schema

--- src/ServerComponents/HandleSchema/GetSchema.js
+++ src/ServerComponents/HandleSchema/GetSchema.js
@@ -1,9 +1,19 @@
 import { join } from 'path';
 import fs from 'fs';
 
-const GetSchema = () => JSON.parse(
-  fs.readFileSync(join(__dirname, '../../ServerFiles/Schema.json'))
-  );
+const GetSchema = () => {
+  if (process.hasOwnProperty('schema')){
+    return process.schema;
+  }
+  else {
+    const readSchema = JSON.parse(
+      fs.readFileSync(join(__dirname, '../../ServerFiles/Schema.json'))
+      );
+    process.schema = readSchema;
+    return process.schema;
+  }
+  
+};
 
 
 
